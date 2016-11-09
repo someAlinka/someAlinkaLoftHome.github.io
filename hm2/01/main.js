a39c31770556f929cf6997244e04cc65b8f91c46
alinatishen@gmail.com <vekmnzirf123>
2016-11-09 22:07:08 +0300

    added the first task on the second homework

---
+++ hm2/01/main.js
@@ -0,0 +1,32 @@
+var allNumbers = [1, 2, 4, 5, 6, 7, 8],
+	someNumbers = [1, 2, 'привет', 4, 5, 'loftschool', 6, 7, 8],
+	noNumbers = ['это', 'массив', 'без', 'чисел'];
+
+function isAllTrue (source, filterFn) {
+	try{
+		if(!source.length) {
+			throw new Error("Пустой массив!!! Oбратитесь к администратору, ему скучно :)");
+		}else{
+			for (var i = 0; i < source.length; i++) {
+				if(!filterFn(source[i])) return false;
+			}
+		}
+		return true;
+	}catch(e){
+		return e.message;
+	}
+}
+
+function isNumber(val) {
+  return typeof val === 'number';
+}
+
+
+
+console.log(isAllTrue(allNumbers, isNumber)); //вернет true
+
+console.log(isAllTrue(someNumbers, isNumber)); //вернет false
+
+console.log(isAllTrue(noNumbers, isNumber)); //вернет false
+
+console.log(isAllTrue([], isNumber)); //вернет сообщение исключения
