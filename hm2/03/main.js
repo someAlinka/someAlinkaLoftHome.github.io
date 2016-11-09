03f3197fc7dfce6c6d261bbf4879187869be77b5
alinatishen@gmail.com <vekmnzirf123>
2016-11-09 22:15:35 +0300

    added a third task in the second homework

---
+++ hm2/03/main.js
@@ -0,0 +1,37 @@
+
+function calculator (firstNumber) {
+	function action(arg, str ){
+		var temp = firstNumber;
+		for (var i = 0; i < arg.length; i++) {
+			temp = eval(temp+str+arg[i])
+		};
+		return temp;
+	}
+	var resObj = {
+		sum: function(){
+			return action(arguments, "+");
+		},
+		dif: function(){
+			return action(arguments, "-");
+		},
+		div: function(){
+			return action(arguments, "/");
+		},
+		mul: function(){
+			return action(arguments, "*");
+		}
+	}
+	return resObj;
+}
+
+var myCalculator = calculator(100);
+
+console.log(myCalculator.sum(1, 2, 3)); //вернет 106
+
+console.log(myCalculator.dif(10, 20)); //вернет 70
+
+console.log(myCalculator.div(2, 2)); //вернет 25
+
+console.log(myCalculator.mul(2, 2)); //вернет 400
+
+console.log(myCalculator.mul()); //вернет 100
