1a5cedbba776a7ce12e54d11c5cc45f36dde9f13
alinatishen@gmail.com <vekmnzirf123>
2016-11-10 19:50:55 +0300

    add exception

--- hm2/03/main.js
+++ hm2/03/main.js
@@ -11,6 +11,7 @@ function calculator (firstNumber) {
 					temp -= arg[i];
 					break;
 				case "/":
+					if(!arg[i]) throw new Error("В школе говорили, что на ноль делить нельзя, значит и мы не будем");
 					temp /= arg[i];
 					break;
 				case "*":
@@ -39,12 +40,18 @@ function calculator (firstNumber) {
 
 var myCalculator = calculator(100);
 
-console.log(myCalculator.sum(1, 2, 3)); //вернет 106
+try{
 
-console.log(myCalculator.dif(10, 20)); //вернет 70
+	console.log(myCalculator.sum(1, 2, 3)); //вернет 106
 
-console.log(myCalculator.div(2, 2)); //вернет 25
+	console.log(myCalculator.dif(10, 20)); //вернет 70
 
-console.log(myCalculator.mul(2, 2)); //вернет 400
+	console.log(myCalculator.div(0, 2)); //вернет 25
 
-console.log(myCalculator.mul()); //вернет 100
+	console.log(myCalculator.mul(2, 2)); //вернет 400
+
+	console.log(myCalculator.mul()); //вернет 100
+
+}catch(e){
+	console.log(e.message);
+}
