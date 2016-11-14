2436f4f91d1b593613f792576816405d02d27973
alinatishen@gmail.com <vekmnzirf123>
2016-11-14 22:58:07 +0300

    fixed first task on the third homework

--- hm3/01/main.js
+++ hm3/01/main.js
@@ -1,20 +1,18 @@
-let array = [1, 2, 3, 4, 5, 6];
+let array = [1, , 3, 4, 5, 6];
 
 function forEach (arr, callback) {
 	if(!arr.length) throw new Error("пустой массив");
 	for (var i = 0; i < arr.length; i++) {
-		callback(arr[i]);
+		if(arr[i] != undefined) callback(arr[i], i, arr);
 	};
 }
 
 function filter (arr, callback) {
 	if(!arr.length) throw new Error("пустой массив");
-	var resArr = [],
-		count = 0;
+	var resArr = [];
 	for (var i = 0; i < arr.length; i++) {
-		if(callback(arr[i])){
-			resArr[count] = arr[i];
-			count++;
+		if(callback(arr[i], i, arr)){
+			resArr[resArr.length] = arr[i];
 		}
 	}
 	return resArr;
@@ -24,7 +22,7 @@ function map (arr, callback) {
 	if(!arr.length) throw new Error("пустой массив");
 	var resArr = [];
 	for (var i = 0; i < arr.length; i++) {
-		resArr[i] = callback(arr[i]);
+		if(arr[i] != undefined) resArr[i] = callback(arr[i], i, arr);
 	}
 	return resArr;
 }
@@ -32,6 +30,8 @@ function map (arr, callback) {
 
 function slice (arr, start = 0, end) {
 	if(!arr.length) throw new Error("пустой массив");
+	start = (start > arr.length)? arr.length : start;
+	start = (start < 0)? arr.length-(-start) : start;
 	var resArr = [];
 	end = (end)? end : arr.length;
 	for (var i = start, j = 0; i < end; i++, j++) {
@@ -41,34 +41,21 @@ function slice (arr, start = 0, end) {
 }
 
 function reduce (arr, callback, initialValue) {
-	var resArr = [];
-	for(var i = 0, j = 0; i < arr.length; i++){ // запись нового массива без пустых значений
-		if(arr[i] === 0 || arr[i]){
-			resArr[j] = arr[i];
-			j++;
-		}
-	}
-	if(!resArr.length && initialValue === undefined) throw new Error("пустой массив и пустой initialValue");
-
-	if(resArr.length == 1 && initialValue === undefined){
-		return resArr[0];
-	}
-
-	if(!resArr.length && initialValue != undefined){
-		return initialValue;
-	}
+	if(!arr.length && initialValue === undefined) throw new Error("пустой массив и пустой initialValue");
+	if(arr.length == 1 && initialValue === undefined) return arr[0];
+	if(!arr.length && initialValue !== undefined) return initialValue;
 
 	var res = 0;
-	for (var k = 0; k < resArr.length; k++) {
+	for (var k = 0; k < arr.length; k++) {
 		if(!k){
-			if(initialValue != undefined){
-				res = callback(initialValue, resArr[k]);
-			}else{
-				res = callback(resArr[0], resArr[1]);
+			if(initialValue !== undefined && arr[k] !== undefined){
+				res = callback(initialValue, arr[k], k, arr);
+			}else if(arr[0] !== undefined && arr[1] !== undefined){
+				res = callback(arr[0], arr[1], k, arr);
 				k++;
 			}
 		}else{
-			res = callback(res, resArr[k]);
+			if(arr[k] !== undefined ) res = callback(res, arr[k], k, arr);
 		}
 	};
 	return res;
@@ -78,7 +65,7 @@ function reduce (arr, callback, initialValue) {
 function splice (start, deleteCount, ...insertItems) {
 	if(!deleteCount && !insertItems.length) return [];
 	if(!array.length && insertItems.length) {
-		array = insertItems
+		array = insertItems;
 		return [];
 	}
 	start = (start > array.length)? array.length : start;
@@ -90,11 +77,11 @@ function splice (start, deleteCount, ...insertItems) {
 	if(deleteCount){	//удаление элементов массива
 		for (var i = 0, j = 0, k = 0; i < array.length; i++) {
 			if(i >= start && i <= deleteCount+start-1){
-				resDelete[k] = array[i];
+				if(array[i] !== undefined) resDelete[k] = array[i];
 				k++;
 				continue;
 			}
-			resArr[j] = array[i];
+			if(array[i] !== undefined) resArr[j] = array[i];
 			j++;
 
 		};
@@ -110,13 +97,11 @@ function splice (start, deleteCount, ...insertItems) {
 					j++;
 				}
 			}
-			if(array[i] == undefined) continue;
-			resArr[j] = array[i];
+			if(array[i] !== undefined) resArr[j] = array[i];
 			j++;
 		};
 	}
 	array = resArr;
-
 	return resDelete;
 }
 
@@ -126,31 +111,35 @@ try{
 	console.log("forEach ___________________________");
 	forEach(array, item => console.log(item));
 
-
 	console.log("filter ___________________________");
 	let greaterThan4 = filter(array, item => item > 4);
 	console.log(greaterThan4);
-
 
 	console.log("map ___________________________");
 	let sqare = map(array, item => item*item);
 	console.log(sqare);
 
-
 	console.log("slice___________________________");
-	console.log(slice(array, 1, 4));
-
+	var a = [1, 2, 3, 4, 5];
+	console.log(a.slice(-1));
+	console.log(slice(a,-1));
 
 	console.log("reduce___________________________");
-	console.log(reduce(array, callbackForReduce, 0));
+	console.log(reduce(array, callbackForReduce, 20));
 	function callbackForReduce(previousValue, currentValue, index, array){
 		return previousValue + currentValue;
 	}
-
-
+	
 	console.log("splice___________________________");
-	console.log(splice(-3, 60, 9 , 9 , 9));
+	var myFish = array;
+	var removed1 = splice(-2, 1, 'hi',null, 6.4, undefined, Infinity);
 	console.log(array);
+	console.log(removed1);
+	console.log('____________________________');
+
+	var removed = myFish.splice(-2, 1, 'hi',null, 6.4, undefined, Infinity);
+	console.log(myFish);
+	console.log(removed);
 
 }catch(e){
 	console.log(e.message);
